b4a7e6ac3a23fb7b6a9634c612ba9b66d5d6c7f5
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-22 15:31:53 -0700

    update routes docs

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * @module src/auth/users-model
+ */
+
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -11,6 +16,17 @@ const SECRET = process.env.SECRET || 'foobar';
 
 const usedTokens = new Set();
 
+const capabilities = {
+  admin: ['create','read','update','delete', 'superuser'],
+  editor: ['create', 'read', 'update'],
+  user: ['read'],
+};
+
+/**
+ * @method Schema
+ * @desc Users schema
+ */
+
 const users = new mongoose.Schema({
   username: {type:String, required:true, unique:true},
   password: {type:String, required:true},
@@ -18,12 +34,22 @@ const users = new mongoose.Schema({
   role: {type: String, default:'user', enum: ['admin','editor','user']},
 }, { toObject:{virtuals:true}, toJSON:{virtuals:true}});
 
+/**
+ * @method Virtual
+ * @desc Users virtual linking to roles
+ */
+
 users.virtual('acl', {
   ref: 'roles',
   localField: 'role',
   foreignField: 'role',
   justOne: true,
 });
+
+/**
+ * @method Pre
+ * @desc Populating the acl table with users roles
+ */
 
 users.pre('findOne', function() {
   try{
@@ -33,13 +59,6 @@ users.pre('findOne', function() {
     console.error('error', e);
   }
 });
-
-
-// const capabilities = {
-//   admin: ['create','read','update','delete'],
-//   editor: ['create', 'read', 'update'],
-//   user: ['read'],
-// };
 
 
 
@@ -52,8 +71,15 @@ users.pre('save', function(next) {
     .catch(error => {throw new Error(error);});
 });
 
+
+/**
+ *   @method createFromOauth
+   * @param {object} googleUser - passed in user info
+   * @desc Create From Oauth function takes in a google users info and creates a user
+   */
+  
 users.statics.createFromOauth = function(googleUser) {
-  console.log('yo');
+
   if(! googleUser) { return Promise.reject('Validation Error'); }
   let email = googleUser.email;
   return this.findOne( {email} )
@@ -64,11 +90,18 @@ users.statics.createFromOauth = function(googleUser) {
     .catch( error => {
       let username = email;
       let password = 'none';
-      let capabilities = 'user';
-      return this.create({username, password, email, capabilities});
+      let role = 'user';
+      return this.create({username, password, email, role});
     });
 
 };
+
+
+/**
+ *   @method authenticateToken
+   * @param {object} token - passed in token
+   * @desc Runs checks to see if current token exists and is valid
+   */
 
 users.statics.authenticateToken = function(token) {
   
@@ -78,12 +111,19 @@ users.statics.authenticateToken = function(token) {
   
   try {
     let parsedToken = jwt.verify(token, SECRET);
+    
     (SINGLE_USE_TOKENS) && parsedToken.type !== 'key' && usedTokens.add(token);
     let query = {_id: parsedToken.id};
     return this.findOne(query);
-  } catch(e) { throw new Error('Invalid Token'); }
+  } catch(e) { console.log('rejecteds'); throw new Error('Invalid Token'); }
   
 };
+
+/**
+ *   @method authenticateBasic
+   * @param {object} auth - passed in authentication/user info
+   * @desc Runs checks to see if current user is in db and is using correct info
+   */
 
 users.statics.authenticateBasic = function(auth) {
   let query = {username:auth.username};
@@ -92,6 +132,12 @@ users.statics.authenticateBasic = function(auth) {
     .catch(error => {throw error;});
 };
 
+
+/**
+ *   @method authenticateBearer
+   * @param {object} token - passed in token
+   * @desc Runs checks to see if current token is valid and can be used to access
+   */
 
 users.statics.authenticateBearer = function(token){
 
@@ -107,33 +153,57 @@ users.statics.authenticateBearer = function(token){
   return this.findOne(query);
 };
 
+/**
+ *   @method comparePassword
+   * @param {object} password - users password
+   * @desc Runs bcrypt to compare passed in password to one on file
+   */
+
 users.methods.comparePassword = function(password) {
   return bcrypt.compare( password, this.password )
     .then( valid => valid ? this : null);
 };
 
-users.methods.generateToken = function(type) {
+/**
+ *   @method generateToken
+   * @param {object} type - type of user, admin, editor, etc.
+   * @desc generates a token if valid and has capability
+   */
 
+users.methods.generateToken = function(type) {
   let token = {
     id: this._id,
-    capabilities: 'admin',
-    type: type || 'user',
+    capabilities: capabilities[this.role],
+    type: type || this.role || 'user',
   };
-  
   let options = {};
   if ( type !== 'key' && !! TOKEN_EXPIRE ) { 
     options = { expiresIn: TOKEN_EXPIRE };
   }
-  
   return jwt.sign(token, SECRET, options);
 };
 
+/**
+ *   @method can
+   * @param {object} capability - users capability
+   * @desc checks to see if the passed in capablity has a certain role
+   */
+
 users.methods.can = function(capability) {
-  return this.acl.capabilities.includes(capability);
+  return capabilities[this.role].includes(capability);
 };
 
+/**
+ *   @method generateKey
+   * @desc generates a key when called
+   */
 users.methods.generateKey = function() {
   return this.generateToken('key');
 };
+
+/**
+ * Export object
+ * @type {Object}
+ */
 
 module.exports = mongoose.model('users', users);
