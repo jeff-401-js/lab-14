74fbe462552850a3d07ab0f6d7534c3673b99963
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-22 15:34:50 -0700

    rolesmodel

--- src/auth/roles-model.js
+++ src/auth/roles-model.js
@@ -2,12 +2,6 @@
 
 const mongoose = require('mongoose');
 
-// const capabilities = {
-//   admin: ['create', 'read', 'update', 'delete', 'superuser'],
-//   editor: ['create', 'read', 'update'],
-//   user: ['read'],
-// };
-
 const rolesSchema = new mongoose.Schema({
   role: {type: String, required:true},
   capabilities: {type: Array, required:true},
